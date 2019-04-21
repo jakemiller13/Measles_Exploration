5e9678877f5ef5890ad45bca0fd377837b0f4a5e
Jake <jakemiller13@hotmail.com>
2019-04-21 17:20:55 -0400

    Update Measles_Exploration.py

--- Measles_Exploration.py
+++ Measles_Exploration.py
@@ -20,23 +20,24 @@ disc = pd.read_excel('measlescasesbycountrybymonth.xls',
 df = pd.read_excel('measlescasesbycountrybymonth.xls',
                    sheet_name = 'WEB')
 df.fillna(0.0, inplace = True)
-print('\nDisclaimer:\n' + disc.iloc[4][1] + '\n')
+print('\nWHO Disclaimer:\n' + disc.iloc[4][1] + '\n')
 
 # Print top 5 lines and first entry to see what data looks like
 print(df.head())
 print('\n' + str(df.iloc[0]))
 
-# How many regions
+# Show unique regions and countires for reference
 regions, regions_counts = np.unique(df['Region'], return_counts = True)
 print('\nRegions:\n' + str(regions))
+countries, countries_counts = np.unique(df['Country'], return_counts = True)
+print('\nCountries:\n' + str(countries))
 
-# Plot cases over time
 def plot_cases(region = None,
                country = None,
                plot_all_years = False,
                year = None):
     '''
-    Plots cases for EITHER "region" -OR- "country" - must specify one OR other
+    Plots cases for EITHER "region" -OR- "country", must specify one OR other
     If "plot_all_years" == True, plots cases over all years
     Otherwise, you must specify "year" to plot
     '''
@@ -52,19 +53,27 @@ def plot_cases(region = None,
         x_plot = np.unique(df['Year'])
         y_plot = [df[(df[loc_col] == location) & (df['Year'] == i)].\
                      iloc[:,4:].sum().sum() for i in x_plot]
-        y_ticks = None
+#        y_ticks = None
     else:
         duration = 'in {}'.format(year)
         x_plot = df.columns[4:]
         y_plot = df[(df[loc_col] == location) & (df['Year'] == year)].\
                     iloc[:,4:].values[0]
-        y_ticks = range(math.floor(min(y_plot)), math.ceil(max(y_plot)) + 1)
+#        y_ticks = range(math.floor(min(y_plot)), math.ceil(max(y_plot)) + 1)
     
     plt.plot(x_plot, y_plot, color = 'red', linewidth = 2)
     plt.grid(which = 'major')
     plt.xticks(rotation = 45, ha = 'right', position = (0, 0.01))
-    plt.yticks(y_ticks)
+#    plt.yticks(y_ticks)
     plt.ylabel('Confirmed Cases')
     plt.title('Measles Cases {} in {}'.format(duration, location))
     plt.show()
-    # TODO make plots pretty
+
+def compare_locations(comp_dict, plot_all_years = False, year = None):
+    '''
+    Compares regions/countries
+    "comp_dict" = dictionary in form {'regions': [], 'countries': []}
+    If "plot_all_years" == True, plots cases over all years
+    Otherwise, you must specify "year" to plot
+    '''
+    
